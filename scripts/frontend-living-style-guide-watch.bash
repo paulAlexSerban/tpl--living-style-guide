bf707cf6c3ed5c52ab8451651dc9af8f72c4c7a8
paulAlexSerban <paul.alex.serban@gmail.com>
2022-07-14 12:09:30 +0300

    ci: wIP

--- scripts/frontend-living-style-guide-watch.bash
+++ scripts/frontend-living-style-guide-watch.bash
@@ -1,5 +1,6 @@
-#!bin/bash
-cd $(dirname $0) # makes sure the folder containing the script will be the root folder
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
 
 echo "👀  Watching Frontend Living Style Guide" 
 npm --prefix ../frontend/living-style-guide run watch
